c40d1d60757eb995aab9accc0f555a5c2c59a665
Yu Kitazume <u.kitazume@gmail.com>
2015-09-28 11:04:22 +0900

    add help test

--- ey-berks.go
+++ ey-berks.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
 func main() {
-	os.Exit(Command(nil))
+	os.Exit(Command(os.Args[1:]))
 }
 
 func Command(argv []string) int {
+
+	usage := `
+Engine Yard Cloud cookbook berkshelf
+Usage: ey-berks <command> [<args>...]
+
+Command:
+
+init     create an EYBerkshelf file
+make     make cookbook directory
+help     show this usage
+`
+
+	if len(argv) == 0 {
+		fmt.Print(usage)
+		return 0
+	}
+
+	fmt.Printf("%s", argv[0])
+
+	switch argv[0] {
+	case "help":
+		fmt.Print(usage)
+		return 0
+	default:
+		fmt.Print(usage)
+		return 0
+	}
 	return 0
 }
--- ey-berks_test.go
+++ ey-berks_test.go
@@ -1,13 +1,51 @@
 package main
 
 import (
+	"bytes"
+	"io"
+	"os"
+	"strings"
 	"testing"
 )
 
+func commandOutput(args []string) (output string) {
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	Command(args)
+
+	outC := make(chan string)
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	w.Close()
+	os.Stdout = old
+	output = <-outC
+	return
+}
+
+func getLine(text string, line int) string {
+	return strings.Split(text, "\n")[line]
+}
+
 func TestNoArgs(t *testing.T) {
-	result := Command(nil)
-	expect := 0
-	if result != expect {
-		t.Errorf("got %v\nwant %v", result, expect)
+	command := []string{""}
+	actual := getLine(commandOutput(command), 1)
+	expect := "Engine Yard Cloud cookbook berkshelf"
+	if actual != expect {
+		t.Errorf("got %v\nwant %v", actual, expect)
 	}
+}
+
+func TestHelp(t *testing.T) {
+	command := []string{"help"}
+	actual := getLine(commandOutput(command), 1)
+	expect := "Engine Yard Cloud cookbook berkshelf"
+	if actual != expect {
+		t.Errorf("Expected %s, Got %s", expect, actual)
+	}
+
 }
