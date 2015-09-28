3f8afaac2b82b90558bf1c63eb9dfc0680a7d585
Yu Kitazume <u.kitazume@gmail.com>
2015-09-28 17:34:34 +0900

    prepare each directory from git

--- gather/gather.go
+++ gather/gather.go
@@ -1,18 +1,68 @@
 package gather
 
 import (
+	"fmt"
+	"github.com/libgit2/git2go"
 	"github.com/ukitazume/ey-berks/config"
+	"os"
+	"path/filepath"
 )
 
 type Gather struct {
-	Berks Berks
+	Berks config.Berks
 }
 
-func NewGather(berksFilePath) Gather {
+func NewGather(berksFilePath string) Gather {
 	bersk := config.Parse(berksFilePath)
-	return &Gather{Berks: bersk}
+	return Gather{Berks: bersk}
+}
+
+func (g *Gather) createWorkingDir() error {
+	err := os.MkdirAll(workingDir(), 0744)
+	if err != nil {
+		return err
+	}
+	err = os.MkdirAll(filepath.Join(workingDir(), "github.com"), 0744)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return nil
 }
 
 func (g *Gather) Gather() error {
-	err := ok.Mkdir("~/.ey-berks")
+	g.createWorkingDir()
+
+	for _, c := range g.Berks.Cookbook {
+		prepareDir(c)
+		updateCookbook(c)
+	}
+	return nil
+}
+
+func workingDir() string {
+	return os.Getenv("HOME") + "/.ey-berks"
+}
+
+func prepareDir(cookbook config.Cookbook) error {
+	path := filepath.Join(workingDir(), "github.com", cookbook.Repo)
+	err := os.MkdirAll(path, 0744)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateCookbook(cookbook config.Cookbook) error {
+	path := filepath.Join(workingDir(), "github.com", cookbook.Repo)
+	if _, err := os.Stat(path + "/.git"); os.IsNotExist(err) {
+		gitCloneOption := new(git.CloneOptions)
+		repo, err := git.Clone("git://github.com/"+cookbook.Repo, path, gitCloneOption)
+		fmt.Println(repo)
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		// add to pull code
+	}
+	return nil
 }
--- gather/gather_test.go
+++ gather/gather_test.go
@@ -1,10 +1,17 @@
 package gather
 
 import (
+	"github.com/ukitazume/ey-berks/config"
 	"testing"
 )
 
 func TestNewGather(t *testing.T) {
+	if err := config.Create("/tmp"); err != nil {
+		t.Errorf("cannot create Berksfile with %d", err)
+	}
 	gather := NewGather("/tmp/EYBerksfile")
-	gather.Gather()
+	err := gather.Gather()
+	if err != nil {
+		t.Errorf("%d", err)
+	}
 }
