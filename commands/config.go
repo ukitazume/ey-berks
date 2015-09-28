f65b6965a6ea4ad0be0ed1f386c9a757b78a5f93
Yu Kitazume <u.kitazume@gmail.com>
2015-09-28 11:40:47 +0900

    command pacakge

---
+++ commands/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"bytes"
+	//"fmt"
+	//#"github.com/BurntSushi/toml"
+	"os"
+)
+
+func Create(path string) error {
+	var fullPath bytes.Buffer
+	fullPath.WriteString(path)
+	fullPath.WriteString("EYBerksfile")
+
+	f, err := os.Create(fullPath.String())
+	if err != nil {
+		return err
+	}
+	defaultFormat := `
+[main]
+libraries = "engineyard/ey-cloud-recipes/main/libraries"
+definitions = "engineyard/ey-cloud-recipes/main/definitions"
+
+[cookbook]
+env_vars = "engineyard/ey-cloud-recipes/cookbooks/env_vars"
+`
+	f.Write([]byte(defaultFormat))
+	f.Close()
+
+	return nil
+}
