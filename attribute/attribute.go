16252cc95cc3c22b0395274b9db273af33a74a2a
Yu Kitazume <u.kitazume@gmail.com>
2015-10-02 17:55:51 +0900

    apply witouht test

--- attribute/attribute.go
+++ attribute/attribute.go
@@ -1,6 +1,7 @@
 package attribute
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -12,7 +13,7 @@ type Options struct {
 }
 
 const (
-	TempDirName = "attributes"
+	TempDirName = "attr-meta"
 )
 
 func DefaultOptions() Options {
@@ -29,14 +30,18 @@ func (opts Options) distPath(path string) string {
 
 func Gather(pathOrignal string, pathDist string, opts Options) error {
 	searchPath := opts.searchPath(pathOrignal)
+	fmt.Println("searching into %v", searchPath)
 	attributes, err := filepath.Glob(searchPath)
 	if err != nil {
 		return err
 	}
-	os.MkdirAll(filepath.Join("./", opts.TempDirName), 0755)
+	if err := os.MkdirAll(filepath.Join(pathDist, opts.TempDirName), 0755); err != nil {
+		return err
+	}
 	for _, attr := range attributes {
 		dirs := strings.Split(attr, "/")
 		newDirName := filepath.Join(opts.distPath(pathDist), dirs[len(dirs)-2])
+		fmt.Printf("find %v\n", newDirName)
 		if err := os.MkdirAll(opts.distPath(pathDist), 0755); err != nil {
 			return err
 		}
@@ -47,7 +52,28 @@ func Gather(pathOrignal string, pathDist string, opts Options) error {
 	return nil
 }
 
-func Apply(path string, opt Options) error {
+func Apply(path string, pathAttributes string, opts Options) error {
+	attributes, err := filepath.Glob(filepath.Join(pathAttributes, "**", "*.rb"))
+	if err != nil {
+		return err
+	}
+	for _, attr := range attributes {
+		dirs := strings.Split(attr, "/")
+		distRecipeDir := filepath.Join(path, "cookbooks", dirs[len(dirs)-2])
+		distDir := filepath.Join(distRecipeDir, "attributes")
+		dist := filepath.Join(distDir, dirs[len(dirs)-1])
+		if _, err := os.Stat(distRecipeDir); os.IsNotExist(err) {
+			fmt.Printf("- skip %v because no recipes in cookbooks\n", attr)
+			continue
+		}
+		if err := os.MkdirAll(distDir, 0755); err != nil {
+			return err
+		}
+		fmt.Printf("+ copy to %v to %v\n", attr, dist)
+		if err := copyFile(attr, dist); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
